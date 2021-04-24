0919ded8959906b00bb5eecab7a0eaa0cc53c63b
1173710209 <47965226+1173710209@users.noreply.github.com>
2021-04-24 12:25:02 +0800

    接雨水，组合总和

--- src/接雨水.java
+++ src/接雨水.java
@@ -1,3 +1,82 @@
 public class 接雨水 {
+    public int trap(int[] height) {
+        int ans = 0;
 
+        int[] calced = new int[height.length];
+
+        if (height.length <= 2) {
+            return ans;
+        }
+
+        int slow = 0;
+        while (height[slow] == 0) {
+            slow++;
+        }
+        int fast = slow + 1;
+
+        while (fast <= height.length - 1) {
+            while (fast <= height.length - 1 && height[fast] < height[slow]) {
+                if (fast == height.length - 1) {
+                    slow = slow + 1;
+                    fast = slow;
+                }
+                fast++;
+            }
+
+            int flag = slow;
+            slow++;
+
+            while (slow != fast) {
+                ans += height[flag] - height[slow];
+                calced[slow] = height[flag] - height[slow];
+                slow++;
+            }
+
+            fast++;
+        }
+
+        //反向
+        slow = height.length - 1;
+        while (height[slow] == 0) {
+            slow--;
+        }
+        fast = slow - 1;
+
+        while (fast >= 0) {
+            while (fast >= 0 && height[fast] < height[slow]) {
+                if (fast == 0) {
+                    slow = slow - 1;
+                    fast = slow;
+                }
+                fast--;
+            }
+
+            int flag = slow;
+            slow--;
+
+            while (slow != fast) {
+                if (calced[slow] < height[flag] - height[slow]) {
+                    ans += height[flag] - height[slow] - calced[slow];
+                }
+                slow--;
+            }
+
+            fast--;
+        }
+
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        //new一个test对象
+        接雨水 test = new 接雨水();
+        //自定义测试用例
+        int[] nums = {0,1,0,2,1,0,1,3,2,1,2,1};
+        long startTime = System.currentTimeMillis();
+        //输出结果
+        test.trap(nums);
+        long endTime = System.currentTimeMillis();
+        System.out.println("程序运行时间：" + (endTime - startTime) + "ms");
+
+    }
 }
--- src/组合总和.java
+++ src/组合总和.java
@@ -1,12 +1,47 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.lang.reflect.Array;
+import java.util.*;
 
 public class 组合总和 {
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        List<List<Integer>> ans = new ArrayList<>();
+        Set<List<Integer>> ans = new HashSet<>();
+        int sum = 0;
+        List<Integer> tmp = new ArrayList<>();
 
+        dfs(ans, sum, target, tmp, candidates);
 
+        return new ArrayList<>(ans);
+    }
 
-        return ans;
+    public void dfs(Set<List<Integer>> ans, int sum, int target, List<Integer> tmp, int[] candidates) {
+        if (sum == target) {
+            List<Integer> sorted = new ArrayList<>(tmp);
+            Collections.sort(sorted);
+            ans.add(sorted);
+            return;
+        }
+
+        for (int i = 0; i < candidates.length; i++) {
+            tmp.add(candidates[i]);
+            sum += candidates[i];
+            if (sum <= target) {
+                dfs(ans, sum, target, tmp, candidates);
+            }
+            sum -= candidates[i];
+            tmp.remove(tmp.size() - 1);
+        }
+    }
+
+    public static void main(String[] args) {
+        //new一个test对象
+        组合总和 test = new 组合总和();
+        //自定义测试用例
+        int[] candidates = {2,3,5};
+        int target = 8;
+        long startTime = System.currentTimeMillis();
+        //输出结果
+        test.combinationSum(candidates, target);
+        long endTime = System.currentTimeMillis();
+        System.out.println("程序运行时间：" + (endTime - startTime) + "ms");
+
     }
 }
